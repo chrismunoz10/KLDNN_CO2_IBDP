54cf73e9d29d29b7f2b8c6b06d291cb898c23721
chrismunoz10 <xianariel10@gmail.com>
2026-09-25 15:57:00 +0300

    Add files via upload

--- DNN_p_all_inputs_2023.py
+++ DNN_p_all_inputs_2023.py
@@ -17,63 +17,42 @@ import pandas as pd
 #change them
 #total ensembled sample size
 NR = 20
-Nxi = 9
-#Neta = 16
 
-#Outputs: MultiKL
-Neta = 3 #Nxyz -> Neta
-Ngamma = 2 #nt -> Ngamma
+if NR == 80:
+    Nxi = 18
+    Neta = 4 #Nxyz -> Neta
+    Ngamma = 3 #nt -> Ngamma
+
+else:
+    Nxi = 9
+    Neta = 3 #Nxyz -> Neta
+    Ngamma = 2 #nt -> Ngamma
 
 #Neural Network
 N_NN = 1 #number of NN training for UQ
 n_epoch = 12_000
 
-# hidden_size = 36
 batch_size = 4 #
 learning_rate = 1e-3
 
-if NR ==20:
-    file_ensemble =  '/home/camunoz4/IBDP_Files/pressures_modified_20.npy'
-else:
-    file_ensemble =  '/home/camunoz4/IBDP_Files/pressures_modified.npy'
 test_case = np.arange(10, NR + 1, step = 10, dtype = int)
 ens_case = np.arange(1, NR + 1, step = 1, dtype = int)
 train_case = np.delete(ens_case, test_case - 1)
 test_size = len(test_case)
 train_size = NR - test_size
 
-p_1 = np.load(file_ensemble)[:NR]
-print(p_1.shape)
 
-Nens, nx, ny, nz, nt = p_1.shape
-Nxy = nx * ny*nz
+nx, ny, nz, nt = [126, 125, 110, 50]
+Nxy = nx * ny * nz
 Nxyz = Nxy
 
-#Split into train / test
-p_1 = np.reshape(p_1, (Nens, Nxy, nt))
-p_1 = np.transpose(p_1, (0, 2, 1)) #(Nens, nt, Nxy)
+p_1_mean = np.load(f'Pressure/p_mean_NR{NR}.npy')
 
-p_1_train = np.delete(p_1, test_case - 1, axis = 0).copy() #(Ntrain, Nxyz)
-p_1_test = p_1[test_case - 1].copy() #(Ntrain, Nxyz)
-p_1_mean = np.mean(p_1_train, axis = 0) #(nt, Nzxy)
-
-del p_1
-
-#p_vw_train = np.delete(p_vw, test_case - 1, axis = 0) #(Ntrain, Nxyz)
-#p_vw_test = p_vw[test_case - 1] #(Ntrain, Nxyz)
-#p_vw_mean = np.mean(p_vw_train, axis = 0) #(Nzxy)
 #Inputs
 #Inputs for all variables at once
-xi_train = np.load(f'inputs_all/xi_all_Nxi{Nxi}_NR{NR}.npy')
-xi_test = np.load(f'inputs_all/xi_all_test_Nxi{Nxi}_NR{NR}.npy')
-"""
-xi_train_xy = np.load(f'inputs/xi_xy_Nxi{Nxi}_NR{NR}.npy')
-xi_test_xy = np.load(f'inputs/xi_xy_test_Nxi{Nxi}_NR{NR}.npy')
-xi_train_z = np.load(f'inputs/xi_z_Nxi{Nxi}_NR{NR}.npy')
-xi_test_z = np.load(f'inputs/xi_z_test_Nxi{Nxi}_NR{NR}.npy')
-xi_train_por = np.load(f'inputs/xi_por_Nxi{Nxi}_NR{NR}.npy')
-xi_test_por = np.load(f'inputs/xi_por_test_Nxi{Nxi}_NR{NR}.npy')
-"""
+xi_train = np.load(f'inputs/xi_all_Nxi{Nxi}_NR{NR}.npy')
+xi_test = np.load(f'inputs/xi_all_test_Nxi{Nxi}_NR{NR}.npy')
+
 #Outputs
 eta_component_train = np.load(f'Pressure/gamma_eta_Neta{Neta}_Ngamma{Ngamma}_NR{NR}.npy') #(Neta, train_size, Ngamma)
 eta_component_test = np.load(f'Pressure/gamma_eta_test_Neta{Neta}_Ngamma{Ngamma}_NR{NR}.npy') #Neta, test_size, Ngamma)
@@ -89,11 +68,6 @@ eta_component_test = np.reshape(eta_component_test, (test_size, -1))
 eta_time_train = np.load(f'Pressure/eta_time_Neta{Neta}_NR{NR}.npy') #(nt, train_size, Neta)
 eta_time_mean = np.mean(eta_time_train, axis = 1) #(nt, Neta)
 
-#eta_train = np.load(f'outputs/xi_s_Nxi{Neta}_NR{NR}.npy')
-#eta_test = np.load(f'outputs/xi_s_test_Nxi{Neta}_NR{NR}.npy')
-#psi_1 = np.load(f'outputs/psi_s_Nxi{Neta}_NR{NR}.npy')
-
-#eigvals_1 = np.load(f'outputs/eigvals_s_NR{NR}.npy')
 # ############################################################### DNN xi -> eta ###################################################
 # Construct Neural Network
 class NN(torch.nn.Module):
@@ -194,7 +168,6 @@ class DNN:
                     
                     # Compute loss
                     loss = criterion(y_pred_train, y)
-                    #loss = DNN.weighted_mse_loss(y_pred_train, y, weights_eta)
                     loss.backward()
                     optimizer.step()
                     running_loss += loss.item()
@@ -250,172 +223,54 @@ class DNN:
         np.save(file_eta_component_test, ensemble_eta_component_test)
            
 ##############################################################################################################################################################
-results = []
-#Conductivity Latent Variables:
-# Nxi_new_list = [4]
-# Neta_dnn_list= [1,2]
-# Ngamma_dnn_list= [3]
-# hidden_size_list=[24]
-# lambda_reg_list=[0]
-Nxi_new_list = [9]
-Neta_dnn_list= [Neta * Ngamma]
-hidden_size_list=[36, 60]
-lambda_reg_list=[0.015, 0.02]
-for lambda_reg in lambda_reg_list:
-    for hidden_size in hidden_size_list:
-        for Nxi_new in Nxi_new_list:
-            for Neta_dnn in Neta_dnn_list:
-                #weights_eta = (Neta_dnn*np.sqrt(eigvals_1[:int(Neta_dnn)])/np.sum(np.sqrt(eigvals_1[:Neta_dnn])))
-                weights_eta = np.ones(Neta_dnn)
-                print(weights_eta)
-                Nxi_dnn=Nxi_new
-                label = f"{lambda_reg}_{hidden_size}_{Nxi_dnn}_{Neta_dnn}"
-                print(f'inputsize{Nxi_dnn}_Netadnn{Neta_dnn}_hiddensize{hidden_size}_lambdareg{lambda_reg}')
-                
-                #input preprocessing 
-                print('xi_train.shape',xi_train.shape)
-                print('xi_test.shape',xi_test.shape)
-                xi_train = xi_train.reshape((train_size, -1))
-                xi_test = xi_test.reshape((test_size, -1))
-                
-                
-                #output preprocessing
-                #eta_train = np.concatenate((eta_train_1[:, :Neta_dnn], eta_train_vw[:, :Neta_dnn]), axis = 1)
-                #eta_test = np.concatenate((eta_test_1[:, :Neta_dnn], eta_test_vw[:, :Neta_dnn]), axis = 1)
-                eta_train = eta_component_train.reshape((train_size, -1))
-                eta_test = eta_component_test.reshape((test_size, -1))
-                print('eta_train.shape',eta_train.shape)
-                print('eta_test.shape',eta_test.shape) 
-                
-                #Use DNN class
-                file_eta_component_train = f'DNN_p_all_inputs/pred_eta_component_Nxi{Nxi_dnn}_Neta{Neta_dnn}_hs{hidden_size}_NR{NR}_lambdareg{lambda_reg}_train.npy'	
-                file_eta_component_test = f'DNN_p_all_inputs/pred_eta_component_Nxi{Nxi_dnn}_Neta{Neta_dnn}_hs{hidden_size}_NR{NR}_lambdareg{lambda_reg}_test.npy'
-                model = DNN(xi_train, xi_test, eta_train, eta_test, hidden_size, lambda_reg, weights_eta, file_eta_component_train, file_eta_component_test) 
-                model.do_DNN()
-                eta_train_pred = np.mean(np.load(file_eta_component_train), axis = 0)  #(train_size,Neta_dnn * Ngamma)
-                eta_test_pred = np.mean(np.load(file_eta_component_test), axis = 0)
-                
-                eta_component_train_pred = np.reshape(np.load(file_eta_component_train), (train_size, Neta, Ngamma))  #it was (train_size, Neta_dnn)
-                eta_component_test_pred = np.reshape(np.load(file_eta_component_test), (test_size, Neta, Ngamma))
-                
-                # eta_components for Test Set
-                errors_eta_component_test = np.zeros((test_size))
-                for i, test in enumerate(test_case):
-                    u_true = eta_test[i]
-                    u_regres = eta_test_pred[i]
-                    error = (u_regres - u_true)**2
-                    errors_eta_component_test[i] = np.sqrt(np.mean(error))
-                avg_eta_component_errors_test = np.mean(errors_eta_component_test, axis=0)
 
-                for i, test in enumerate(test_case):
-                    plt.figure()
-                    plt.scatter(np.arange(len(eta_test[i])), eta_test[i], marker='o', label='True')
-                    plt.scatter(np.arange(len(eta_test_pred[i])), eta_test_pred[i], marker='x', label='Prediction')
-                    plt.legend()
-                    plt.title('Eta_component Comparison')
-                    plt.savefig(f'DNN_p_all_inputs/eta_component_s_comparison_Nxi{Nxi_dnn}_Neta{Neta_dnn}_hs{hidden_size}_NR{NR}_lambdareg{lambda_reg}_test{test}.png', dpi=300)
-                    plt.close() 
+#Hyperparameters for DNN Training
+if NR == 80:
+    Nxi_dnn = 18
+else:
+    Nxi_dnn = 9
+Neta_dnn = Neta * Ngamma
+if NR == 80:
+    hidden_size = 36
+else:
+    hidden_size = 60
+lambda_reg = 0.02
 
-                # eta_components for Train Set
-                errors_eta_component_train = np.zeros((train_size))
-                for i, train in enumerate(train_case):
-                    u_true = eta_train[i]
-                    u_regres = eta_train_pred[i]
-                    error = (u_regres - u_true)**2
-                    errors_eta_component_train[i] = np.sqrt(np.mean(error))
-                avg_eta_component_errors_train = np.mean(errors_eta_component_train, axis=0)
+#Output directories and filename
+file_eta_component_train = f'DNN_p_all_inputs/pred_eta_component_Nxi{Nxi_dnn}_Neta{Neta_dnn}_hs{hidden_size}_NR{NR}_lambdareg{lambda_reg}_train.npy'	
+file_eta_component_test = f'DNN_p_all_inputs/pred_eta_component_Nxi{Nxi_dnn}_Neta{Neta_dnn}_hs{hidden_size}_NR{NR}_lambdareg{lambda_reg}_test.npy'
 
-                plt.figure()
-                plt.scatter(np.arange(len(eta_train[0])), eta_train[0], marker='o', label='True')
-                plt.scatter(np.arange(len(eta_train_pred[0])), eta_train_pred[0], marker='x', label='Prediction')
-                plt.legend()
-                plt.title('Eta_component Comparison')
-                plt.savefig(f'DNN_p_all_inputs/eta_component_s_comparison_Nxi{Nxi_dnn}_Neta{Neta_dnn}_hs{hidden_size}_NR{NR}_lambdareg{lambda_reg}_train.png', dpi=300)
-                plt.close()
-                
-                # Prediction for Testing Set
-                p_test_pred = np.zeros((test_size, nt, Nxy))
-                eta_rec_test = np.zeros((nt, test_size, Neta))
-                for j in range(Neta):
-                    eta_rec_test[:, :, j] = (eta_time_mean[:, j] + (psi_component[j, :, :] @ eta_component_test_pred[:, j, :].T).T).T   
-                
-                for i in range(nt):
-                    p_test_pred[:, i, :] = p_1_mean[i, :] + (psi_time[i, :, :Neta] @ eta_rec_test[i, :, :].T).T
-                    
-                # Calculate Errors - TESTING CCS1
-                errors_linear_test = np.zeros((test_size, 4))
-                for i, test in enumerate(test_case):
-                    u_true = p_1_test[i]
-                    u_regres = p_test_pred[i]
-                    error = (u_regres - u_true)**2
-                    error_mean = (p_1_mean - u_true)**2
-                    errors_linear_test[i, 0] = np.sqrt(np.mean(error))
-                    errors_linear_test[i, 1] = np.sqrt(np.mean(error_mean))
-                    errors_linear_test[i, 2] = np.max(np.mean(np.abs(error)))
-                    errors_linear_test[i, 3] = np.max(np.mean(np.abs(error_mean)))
-                avg_errors_test = np.mean(errors_linear_test, axis=0)
-                np.save(f'DNN_p_all_inputs/errors_s_Nxi{Nxi_dnn}_Neta{Neta_dnn}_hs{hidden_size}_NR{NR}_lambdareg{lambda_reg}_test.npy', errors_linear_test)
-                # Plotting TESTING Errors
-                plt.figure(dpi=300)
-                plt.scatter(test_case, errors_linear_test[:, 1], label='Average RMSE', color='blue', marker='o')
-                plt.scatter(test_case, errors_linear_test[:, 0], label='DNN RMSE', color='red', marker='o')
-                plt.scatter(test_case, errors_linear_test[:, 3], label='Average MAE', color='purple', marker='x')
-                plt.scatter(test_case, errors_linear_test[:, 2], label='DNN MAE', color='crimson', marker='x')
-                plt.xlabel('Test Case')
-                plt.ylabel('Pressure Error (psi)')
-                plt.legend()
-                plt.savefig(f'DNN_p_all_inputs/errors_s_Nxi{Nxi_dnn}_Neta{Neta_dnn}_hs{hidden_size}_NR{NR}_lambdareg{lambda_reg}_test.png')
-                plt.close()
-                del p_test_pred
-                
-                #Prediction for Training Set
-                # Prediction for Testing Set
-                p_train_pred = np.zeros((train_size, nt, Nxy))
-                eta_rec_train = np.zeros((nt, train_size, Neta))
-                for j in range(Neta):
-                    eta_rec_train[:, :, j] = (eta_time_mean[:, j] + (psi_component[j, :, :] @ eta_component_train_pred[:, j, :].T).T).T   
-                
-                for i in range(nt):
-                    p_train_pred[:, i, :] = p_1_mean[i, :] + (psi_time[i, :, :Neta] @ eta_rec_train[i, :, :].T).T
-                    
+weights_eta = np.ones(Neta_dnn)
+print(weights_eta)
+label = f"{lambda_reg}_{hidden_size}_{Nxi_dnn}_{Neta_dnn}"
+print(f'inputsize{Nxi_dnn}_Netadnn{Neta_dnn}_hiddensize{hidden_size}_lambdareg{lambda_reg}')
 
-                 # Calculate Errors - TRAINING CCS1
-                errors_linear_test = np.zeros((train_size, 4))
-                for i, test in enumerate(train_case):
-                    u_true = p_1_train[i]
-                    u_regres = p_train_pred[i]
-                    error = (u_regres - u_true)**2
-                    error_mean = (p_1_mean - u_true)**2
-                    errors_linear_test[i, 0] = np.sqrt(np.mean(error))
-                    errors_linear_test[i, 1] = np.sqrt(np.mean(error_mean))
-                    errors_linear_test[i, 2] = np.max(np.mean(np.abs(error)))
-                    errors_linear_test[i, 3] = np.max(np.mean(np.abs(error_mean)))
-                avg_errors_test = np.mean(errors_linear_test, axis=0)
-                np.save(f'DNN_p_all_inputs/errors_s_Nxi{Nxi_dnn}_Neta{Neta_dnn}_hs{hidden_size}_NR{NR}_lambdareg{lambda_reg}_train.npy', errors_linear_test)
-                # Plotting TESTING Errors
-                plt.figure(dpi=300)
-                plt.scatter(train_case, errors_linear_test[:, 1], label='Average RMSE', color='blue', marker='o')
-                plt.scatter(train_case, errors_linear_test[:, 0], label='DNN RMSE', color='red', marker='o')
-                plt.scatter(train_case, errors_linear_test[:, 3], label='Average MAE', color='purple', marker='x')
-                plt.scatter(train_case, errors_linear_test[:, 2], label='DNN MAE', color='crimson', marker='x')
-                plt.xlabel('Train Case')
-                plt.ylabel('Pressure Error (psi)')
-                plt.legend()
-                plt.savefig(f'DNN_p_all_inputs/errors_s_Nxi{Nxi_dnn}_Neta{Neta_dnn}_hs{hidden_size}_NR{NR}_lambdareg{lambda_reg}_train.png')
-                plt.close()
-                
-                del p_train_pred
-                """
-                #apend to results list
-                #results.append([label, avg_errors_train[0],avg_errors_train[1],avg_errors_test[0],avg_errors_test[1],avg_eta_component_errors_train,avg_eta_component_errors_test])
-#print(weights_eta)
+#input preprocessing 
+xi_train = xi_train.reshape((train_size, -1))
+xi_test = xi_test.reshape((test_size, -1))
 
+#output preprocessing
+eta_train = eta_component_train.reshape((train_size, -1))
+eta_test = eta_component_test.reshape((test_size, -1))
 
-               
-# Convert results to DataFrame
-results_df = pd.DataFrame(results, columns=["Label", "Train_DNN_RMSE", "Train_Avg_RMSE", "Test_DNN_RMSE", "Test_Avg_RMSE","Train_eta_component_RMSE","Test_eta_component_RMSE"])
+#Use DNN class
 
-# Write to Excel file
-results_df.to_excel("p_errors.xlsx", index=False)
-print("Error metrics saved to 'p_errors.xlsx'")
-"""
+model = DNN(xi_train, xi_test, eta_train, eta_test, hidden_size, lambda_reg, weights_eta, file_eta_component_train, file_eta_component_test) 
+model.do_DNN()
+eta_train_pred = np.mean(np.load(file_eta_component_train), axis = 0)  #(train_size,Neta_dnn * Ngamma)
+eta_test_pred = np.mean(np.load(file_eta_component_test), axis = 0)
+
+eta_component_train_pred = np.reshape(np.load(file_eta_component_train), (train_size, Neta, Ngamma))  #it was (train_size, Neta_dnn)
+eta_component_test_pred = np.reshape(np.load(file_eta_component_test), (test_size, Neta, Ngamma))
+
+# Prediction for Testing Set
+p_test_pred = np.zeros((test_size, nt, Nxy))
+eta_rec_test = np.zeros((nt, test_size, Neta))
+for j in range(Neta):
+    eta_rec_test[:, :, j] = (eta_time_mean[:, j] + (psi_component[j, :, :] @ eta_component_test_pred[:, j, :].T).T).T   
+
+for i in range(nt):
+    p_test_pred[:, i, :] = p_1_mean[i, :] + (psi_time[i, :, :Neta] @ eta_rec_test[i, :, :].T).T
+
+#Reshape into original dimensions:
+p_test_pred =  np.reshape(np.transpose(p_test_pred, (0, 2, 1)), (test_size, nx, ny, nz, nt)) 
